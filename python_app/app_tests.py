83bdd711d586629286141db447e7c5198d7d4d11
NicolaC41! <nicolasmith2004@hotmail.com>
2025-05-14 10:54:28 +0100

    Add your file

---
+++ python_app/app_tests.py
@@ -0,0 +1,13 @@
+import unittest
+import pandas as pd
+from app_refactored import 
+
+#Add yourfunctions that you want to test
+
+# At a minumum test these:
+## Check that the duration column (between checout and eturn) is an integer
+## check that the same column above zero
+## do any other function you've created (or mine)
+
+
+
